2071f50993b15009a5c467b4ffc7b96b3d6e4532
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2023-12-06 20:22:24 +0530

    Escape

---
+++ escape sequence.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+# include<conio.h>
+int main()
+{
+  printf("\n welcome to C programing....");
+  printf("\n\n \'KARAD\'\"FORK\"");
+  getch ();
+  printf("\a");
+  printf("\n\n\t\tBYEBYE....");
+  getch ();
+}
