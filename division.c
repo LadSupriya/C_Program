c99b3dedaedf16da0e8c50f1da0d001ec1010d42
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2023-12-06 20:08:54 +0530

    Division

---
+++ division.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, div;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    div = number1 / number2;      
+    
+    printf("%d / %d = %d", number1, number2, div);
+    return 0;
+}
