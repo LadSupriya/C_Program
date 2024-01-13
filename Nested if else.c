914ed9834e04f0d2a8dc219caf258f291791e33a
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-01-13 20:43:34 +0530

    Nested if else

---
+++ Nested if else.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+  
+
+int main() 
+{ 
+
+    int i = 10; 
+
+  
+
+    if (i > 15) { 
+
+        printf("10 is greater than 15"); 
+
+    } 
+
+  
+
+    printf("I am Not in if"); 
+}
