e19d291eb46268912d03ab2a35f64c040c33b6f5
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-01-17 20:53:50 +0530

    Addition

---
+++ sum of 5 no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a=50;
+  int b=25;
+  int c=75;
+  int d=15;
+  int e=75;
+  int sum;
+  sum=a+b+c;
+  printf("sum of given number is %d",sum);
+  return 0;
+}
