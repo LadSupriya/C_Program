cf5bb01ad70e7bc2464fa72933886ca6933a0d69
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-01-19 11:01:11 +0530

    Mean of sum

---
+++ mean of sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int a=10;
+  int b=15;
+  int c=30;
+  int sum;
+  int mean;
+  sum=a+b+c;
+  printf("sum of the given numbers is%d",sum);
+  mean=sum/3;
+  printf("\n mean of given number is%d",mean);
+  return 0;
+  }
