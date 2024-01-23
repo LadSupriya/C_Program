cc9fae3caaa41544eeaf1cff68e79455e5360cc5
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-01-23 13:03:50 +0530

    Circumference

---
+++ circumference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int r=7;
+  float c;
+  printf("\n enter radius of circle");
+  scanf("%d",&r);
+  c=2*3.14*r;
+  printf("circumference of circle is%f",c);
+  return 0;
+}
