873585fad73ea8d6634c9acf454d3e20cb1aa500
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-01-24 17:13:35 +0530

    Volume of box

---
+++ box.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int l=4, b=5, h=7, v;
+  printf("\n enter length, nteadth and height");
+  scanf("%d%d%d",&l,&b,&h);
+  v=l*b*h;
+  printf("volume of box is%d",v);
+  return 0;
+}
