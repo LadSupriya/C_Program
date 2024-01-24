6b6e80f95bc1327057a31dc183e54b2795303078
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-01-24 21:59:19 +0530

    Square

---
+++ square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int n, square;
+  printf("\n enter a number");
+  scanf("%d",&n);
+  square=n*n;
+  printf("square of given number is%d",square);
+  return 0;
+}
