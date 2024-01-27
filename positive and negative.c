aa94168e5a1ad42222b9daa3a70dbb322213f794
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-01-27 17:39:09 +0530

    Positive and negative

---
+++ positive and negative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+  int n;
+  printf("enter any number");
+  scanf("%d",&n);
+  if(n>0)
+  {
+    printf("\n number is positive");
+  }
+  else
+  {
+    printf("\n number is negative");
+  }
+  return 0;
+}
