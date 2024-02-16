abce3ab61a1e52b24330297c8fab4185787b102b
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-02-16 18:28:46 +0530

    Add files via upload

---
+++ sss.c
@@ -0,0 +1,12 @@
+//wap to print area of circle
+#include<stdio.h>
+int main()
+{
+  int r;
+  float ac;
+  printf("Enter the redius");
+  scanf("%d",&r);
+  ac=3.14*r*r;
+  printf("\n Area of circle is%f",ac);
+  return 0;
+}
---
+++ supriya.c
@@ -0,0 +1,12 @@
+//wap to print all even number from 20 to 2 in reverse order
+#include<stdio.h>
+int main()
+{
+ int n;
+ for(n=20;n>=2;n=n-2) 
+  {
+    printf("\n %d",n);
+   
+  }
+   return 0;
+}
