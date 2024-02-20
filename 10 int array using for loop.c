fd327366053919351faf2b36239dbcb72aabc319
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-02-20 08:04:46 +0530

    Array

---
+++ 10 int array using for loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int a[10]={43,66,37,87,44,35,97,78,69,34};
+  int i;
+  for(i=0;i<=9;i++)
+  {
+    printf("\n%d",a[i]);
+  }
+  
+  return 0;
+}
---
+++ 7 float array using for loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  float a[7]={5.0,3.0,8.0,5.0,4.0,6.0,7.0};
+  int i;
+  for(i=0;i<=6.0;i++)
+  {
+    printf("\n%f",a[i]);
+  }
+  return 0;
+}
