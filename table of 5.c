f3890f7a2ba24bdcc7720a156dd53342235365db
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-03-02 19:15:56 +0530

    Add files via upload

---
+++ table of 5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int i=1;
+  while(i<=10)
+  {
+    
+    printf("\n%d\t%d",i*6,i*7);
+    i++;
+  }
+  
+  
+  return 0;
+}
---
+++ using while loop reverse table 5and8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int i=10;
+  while(i>=1)
+  {
+    printf("\n%d,\t%d",i*5,i*8);
+     i--;
+     }
+  return 0;
+}
