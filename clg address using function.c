7eec63ade301205bfeab6ca8705f91b442f178c6
LadSupriya <149806963+LadSupriya@users.noreply.github.com>
2024-03-08 19:37:38 +0530

    Function

---
+++ clg address using function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void collage()
+{
+  printf("\n Balasaheb Desai Collage");
+  printf("\n Patan 415206");
+}
+int main()
+{
+  printf("\n Supriya lad");
+  collage();
+  printf("\n Harshada chavan");
+  collage();
+}
---
+++ function.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void company()
+{
+printf("\nSJ IT Training Institute");
+printf("\nvidyanagar saidapur");
+printf("\nkarad 415124");
+}
+int main()
+{
+  printf("\nsupriya lad");
+  company();
+  printf("\nharshada chavan");
+  company();
+}
